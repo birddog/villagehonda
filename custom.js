7e310b98338607fb11b108f5d354b89b909d5bf2
Chad Payne <chad@bird-dog.ca>
2010-06-24 16:54:08 -0600

    disabled hover on showcaseslider now uses vhover

--- custom.js
+++ custom.js
@@ -544,10 +544,11 @@ jQuery(function($) {
       $('#showcase-flyout .showcase-pane').prepend('<div class="showcase-prev"></div>');
       $('#showcase-flyout .showcase-pane').append('<div class="showcase-next"></div>');
       $('#showcase-flyout .showcase-pane').scrollable({speed:100, size:5, clickable:false, items:'.items', next:'.showcase-next', prev:'.showcase-prev', item:'.vehicle'});
-      $('#showcase-flyout .showcase-pane .vehicle').hover(
+/*      Commented out so jQuery vHover plugin takes over.
+	$('#showcase-flyout .showcase-pane .vehicle').hover(
         function(){ $(this).children('.trims').show(); },
         function(){ $(this).children('.trims').hide(); }
-      );
+      );*/
       $('#showcase-flyout .showcase-tabs').tabs('#showcase-flyout > .showcase-pane');
     }
     $('.page-item-151').hover(function(){
