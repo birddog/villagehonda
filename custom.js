c0dc8f40e6c31a247c23587664ea10c05950b4d4
Chad Payne <chad@bird-dog.ca>
2010-06-25 11:29:06 -0600

    Vhover showcaseslider tweak

--- custom.js
+++ custom.js
@@ -1420,11 +1420,11 @@ addLoadEvent(initLightbox);	// run initLightbox onLoad
 			var vehicle = base.$el.find('.vehicle');				// the items to clone and make hovers out of
 			var baseDisplay = base.$el.css('display');	// Get current display setting of showcase to revert to
 
-			// Take showcase and display off screen to obtain size and positions
-			base.$el.css({left:'-10000px'}).show();
-
 			// Loop through items to make clones and set events
 			vehicle.each(function(index, value) {
+				// Take showcase and display off screen to obtain size and positions
+				base.$el.css({left:'-10000px'}).show();
+
 				var $this = $(this); // .vehicle
 				var position = $this.position();
 				var clone = $this.clone();
