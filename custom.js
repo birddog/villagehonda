c120c4920f2ddfdd8e73a98a80fb6de7528fe484
Chad Payne <chad@bird-dog.ca>
2010-06-29 18:06:56 -0600

    Added flyout hover and vhover shows flyout

--- custom.js
+++ custom.js
@@ -549,7 +549,7 @@ jQuery(function($) {
       );*/
       $('#showcase-flyout .showcase-tabs').tabs('#showcase-flyout > .showcase-pane');
     }
-	$('.page-item-151').add('#showcase-flyout').hover(function(){
+	$('.page-item-151').add('#showcase-flyout').add('.vhover').hover(function(){
 		$('#showcase-flyout').show();
 	} , function() {
 		$('#showcase-flyout').hide();
