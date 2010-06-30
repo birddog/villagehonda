9cd08cf8d64cdfb1ea6647eaee6223af3dc007dd
Chad Payne <chad@bird-dog.ca>
2010-06-30 16:19:35 -0600

    changed vhover position

--- custom.js
+++ custom.js
@@ -1467,7 +1467,7 @@ addLoadEvent(initLightbox);	// run initLightbox onLoad
 						var top = position.top;
 						break;
 					case 2:
-						var left = position.left;
+						var left = position.left + 5;
 						var top = '34px';
 						break;
 				}				
