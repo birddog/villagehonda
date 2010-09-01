638dc6e15ac24e40baa6cf7a049de82b2c7f745e
Chad Payne <chad@bird-dog.ca>
2010-09-01 15:28:53 -0600

    drop down menu implementation

--- header.php
+++ header.php
@@ -26,9 +26,11 @@
 
       <?php $dm_page = get_page_by_title('Directory Menu'); ?>
       <div id="menubar" class="grid_12 alpha omega">
+        <div class="nav">
           <?php
             //wp_list_pages('child_of='.$dm_page->ID.'&depth=1&title_li=&sort_column=menu_order');
             wp_nav_menu(array('sort_column' => 'menu_order', 'container_class' => 'menu-navigation' ) );
           ?>
+        </div>
       </div>
       <div class="clear"></div>
